eedf5416cfa4db3c264e52ffb6ad2fee5ce8d82f
AdhamSamer3301 <adhamsamer3301@gmail>
2021-09-06 15:16:41 +0200

    Lecture4, JS

---
+++ JavaScript/Test/index.js
@@ -0,0 +1,27 @@
+/*var myName="Adham", myCountry="Alexandria", myAge=20;
+var first = 100, second = 200 , res = first + second;
+
+var x=3, y= 5, z = x+y;
+document.getElementById("js").innerHTML = myName + "<br>" + myAge + "<br>" + myCountry;
+document.getElementById("js2").innerHTML = z;
+console.log(z);
+alert("HelloWorld !");*/
+
+/*var hasDiscount = false;
+if (hasDiscount===true) {
+    mainPrice = 350;
+}
+else{
+    mainPrice = 450;
+}
+//console.log(mainPrice);
+document.getElementById("js").innerHTML =mainPrice;*/
+
+Object={
+    name:" 'Adham' ",
+    Age:20
+}
+console.log(Object);
+document.getElementById("js").innerHTML =Object.name;
+
+
